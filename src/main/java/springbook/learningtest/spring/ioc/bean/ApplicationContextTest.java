fbf3cc5a07e03c258a1ad1afe35d69fb7ff79a41
namyul <bny64@naver.com>
2024-02-15 23:23:04 +0900

    2024.02.15 토비의 스프링 1장 IoC 컨테이너와 DI

--- src/main/java/springbook/learningtest/spring/ioc/bean/ApplicationContextTest.java
+++ src/main/java/springbook/learningtest/spring/ioc/bean/ApplicationContextTest.java
@@ -1,10 +1,21 @@
 package springbook.learningtest.spring.ioc.bean;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "springbook/learningtest/spring/ioc/genericApplicationContext.xml")
 public class ApplicationContextTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
 
     @Test
     public void genericApplicationContext() {
@@ -18,5 +29,13 @@ public class ApplicationContextTest {
         String msg = hello.printer.toString();
         System.out.println(msg);
 
+    }
+
+    @Test
+    public void genericXmlApplicationContextTest() {
+        Hello hello = applicationContext.getBean("hello", Hello.class);
+        hello.print();
+        String msg = hello.printer.toString();
+        System.out.println(msg);
     }
 }
